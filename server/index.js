8451a8bd6ef95441f2c28e19a0eb6f037d6027c0
Jaswinder Singh <55909025+jaswindersingh97@users.noreply.github.com>
2025-01-03 12:22:44 +0530

    basic setup

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require("express"); 
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const cors = require("cors");
+app.use(cors());
+
+const connectDB = require("./config/db");
+connectDB();
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.status || 500).json({ message: err.message || 'Internal Server Error' });
+});
+
+const requestLogger = require('./middleware/requestLogger');
+app.use(requestLogger)
+
+const authEndpoints = require("./endpoints/authEnpoints");
+app.use(authEndpoints);
+
+const authMiddleware = require("./middleware/authMiddleware");
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the server" });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("Server is running on port 3000");
+});
